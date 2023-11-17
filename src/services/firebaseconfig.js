3174fc0aba41e914977d61a15cf87a983b3d16be
srfreitass <guilhermefreitasdonascimento67@gmail.com>
2023-11-17 19:10:39 -0400

    credentials firebase

--- src/services/firebaseconfig.js
+++ src/services/firebaseconfig.js
@@ -6,14 +6,14 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_DATABASE_URL,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDuGBzS1FLktJu84-1ql9ZoNUY-en1Kx58",
+  authDomain: "techconnectdb.firebaseapp.com",
+  databaseURL: "https://techconnectdb-default-rtdb.firebaseio.com",
+  projectId: "techconnectdb",
+  storageBucket: "techconnectdb.appspot.com",
+  messagingSenderId: "615342452845",
+  appId: "1:615342452845:web:f52b493b58716aeb250f64",
+  measurementId: "G-6SB4TSG0BG",
 };
 
 // Initialize Firebase
