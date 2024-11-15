956f77164731f321d2a59558f7db7e68b05174b8
srfreitass <guilhermefreitasdonascimento67@gmail.com>
2024-11-15 13:44:07 -0400

    chore: add dotenv

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,24 +1,23 @@
 // Import the functions you need from the SDKs you need
-import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDuGBzS1FLktJu84-1ql9ZoNUY-en1Kx58",
-  authDomain: "techconnectdb.firebaseapp.com",
-  databaseURL: "https://techconnectdb-default-rtdb.firebaseio.com",
-  projectId: "techconnectdb",
-  storageBucket: "techconnectdb.appspot.com",
-  messagingSenderId: "615342452845",
-  appId: "1:615342452845:web:f52b493b58716aeb250f64",
-  measurementId: "G-6SB4TSG0BG",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
