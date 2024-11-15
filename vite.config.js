83a5f67257d4de97dcd88af667beafa3e8d77b6e
srfreitass <guilhermefreitasdonascimento67@gmail.com>
2024-11-15 15:05:59 -0400

    chore: vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV)
+    },
+    plugins: [react()],
+    server: {
+      host: true
+    }
   }
 })
